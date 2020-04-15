544785cb7393921dd10ac5b753a13eb5836cdf4e
Francesc Verdugo <fverdugo@cimne.upc.edu>
2020-04-15 07:48:02 +0200

    Allowing unary nodes

--- src/CSG/Nodes.jl
+++ src/CSG/Nodes.jl
@@ -7,7 +7,13 @@ end
 
 Base.eltype(::Type{<:Node{Td}}) where Td = Td
 
+const UnaryNode = Node{Td,Tc,Nothing} where {Td,Tc}
+
 const Leaf = Node{Td,Nothing,Nothing} where Td
+
+function Node(data,leftchild)
+  Node(data,leftchild,nothing)
+end
 
 function Leaf(data)
   Node(data,nothing,nothing)
@@ -15,6 +21,10 @@ end
 
 function children(a::Node)
   (a.leftchild,a.rightchild)
+end
+
+function children(a::UnaryNode)
+  (a.leftchild,)
 end
 
 function children(a::Leaf)
@@ -28,6 +38,14 @@ function Base.show(io::IO,a::Node)
   show(io,a.leftchild)
   print(io,",")
   show(io,a.rightchild)
+  print(io,")")
+end
+
+function Base.show(io::IO,a::UnaryNode)
+  print(io,"Node(")
+  show(io,a.data)
+  print(io,",")
+  show(io,a.leftchild)
   print(io,")")
 end
 
@@ -54,6 +72,12 @@ function replace_data(filter,a::Node)
   Node(data,c1,c2)
 end
 
+function replace_data(filter,a::UnaryNode)
+  data = filter(a.data)
+  c1 = replace_data(filter,a.leftchild)
+  Node(data,c1)
+end
+
 function replace_data(filter,a::Leaf)
   data = filter(a.data)
   Leaf(data)
@@ -64,6 +88,12 @@ function replace_data(filter_node,filter_leaf,a::Node)
   c1 = replace_data(filter_node,filter_leaf,a.leftchild)
   c2 = replace_data(filter_node,filter_leaf,a.rightchild)
   Node(data,c1,c2)
+end
+
+function replace_data(filter_node,filter_leaf,a::UnaryNode)
+  data = filter_node(a.data)
+  c1 = replace_data(filter_node,filter_leaf,a.leftchild)
+  Node(data,c1)
 end
 
 function replace_data(filter_node,filter_leaf,a::Leaf)
--- test/CSGTests/NodesTests.jl
+++ test/CSGTests/NodesTests.jl
@@ -11,12 +11,19 @@ c = Leaf("c")
 d = Node("d",a,b)
 e = Node("e",c,d)
 f = Node("f",e,b)
+g = Node("g",f)
 
 #print_tree(stdout,f)
+#print_tree(stdout,g)
 
 h = replace_data(objectid,f)
 for (fi,hi) in zip(PreOrderDFS(f),PreOrderDFS(h))
   @test objectid(fi.data) == hi.data
+end
+
+h = replace_data(objectid,g)
+for (gi,hi) in zip(PreOrderDFS(g),PreOrderDFS(h))
+  @test objectid(gi.data) == hi.data
 end
 
 h = replace_data(d->d,objectid,f)
@@ -28,6 +35,17 @@ for (fi,hi) in zip(PreOrderDFS(f),PreOrderDFS(h))
   end
 end
 
+h = replace_data(d->d,objectid,g)
+for (gi,hi) in zip(PreOrderDFS(g),PreOrderDFS(h))
+  if isa(gi,Leaf)
+    @test objectid(gi.data) == hi.data
+  else
+    @test gi.data === hi.data
+  end
+end
+
 leaves = collect(Leaves(f))
+
+leaves = collect(Leaves(g))
 
 end # module
