8a0c222075944d0054d739940a013fa6c9dda7d5
eneiva <eneiva@cimne.upc.edu>
2020-09-01 12:43:27 +0200

    Move all GridapEmbeddedTests into a separate job

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,15 +7,13 @@ using MiniQhull
 if MiniQhull.QHULL_WRAPPER_LOADED[]
 
   @time @testset "CSG" begin include("CSGTests/runtests.jl") end
-  
+
   @time @testset "Interfaces" begin include("InterfacesTests/runtests.jl") end
-  
+
   @time @testset "LevelSetCutters" begin include("LevelSetCuttersTests/runtests.jl") end
 
   @time @testset "AgFEM" begin include("AgFEMTests/runtests.jl") end
-  
-  @time @testset "GridapEmbedded" begin include("GridapEmbeddedTests/runtests.jl") end
-  
+
   include(joinpath(@__DIR__,"..","examples","runexamples.jl"))
 
 else
