70a05e2d5c59f4ba2e2f08ea1292a9f1b97a6f7a
Pere Antoni Martorell <pmartorell@cimne.upc.edu>
2024-04-12 14:45:02 +0200

    [distributed] setup mpi tests

---
+++ test/DistributedTests/mpi/runtests_body.jl
@@ -0,0 +1,39 @@
+module MPITestsBody
+
+using PartitionedArrays
+const PArrays = PartitionedArrays
+using MPI
+
+include("../PoissonTests.jl")
+
+if ! MPI.Initialized()
+  MPI.Init()
+end
+
+function all_tests(distribute,parts)
+  ranks = distribute(LinearIndices((prod(parts),)))
+
+  t = PArrays.PTimer(ranks,verbose=true)
+  PArrays.tic!(t)
+
+  PoissonTests.main(distribute,parts)
+  PoissonTests.main(distribute,(prod(parts),1),cells=(12,12),geometry=:remotes)
+
+  PArrays.toc!(t,"Geometry")
+
+  display(t)
+end
+
+if MPI.Comm_size(MPI.COMM_WORLD) == 4
+  with_mpi() do distribute
+    all_tests(distribute,(2,2))
+  end
+elseif MPI.Comm_size(MPI.COMM_WORLD) == 1
+  with_mpi() do distribute
+    all_tests(distribute,(1,1))
+  end
+else
+  MPI.Abort(MPI.COMM_WORLD,0)
+end
+
+end #module
--- test/DistributedTests/sequential/PoissonTests.jl
+++ test/DistributedTests/sequential/PoissonTests.jl
@@ -3,6 +3,6 @@ using PartitionedArrays
 include("../PoissonTests.jl")
 with_debug() do distribute
     PoissonTests.main(distribute,(2,2))
-    PoissonTests.main(distribute,(4,1),cells=(9,9),geometry=:remotes)
+    PoissonTests.main(distribute,(4,1),cells=(12,12),geometry=:remotes)
 end
 end
