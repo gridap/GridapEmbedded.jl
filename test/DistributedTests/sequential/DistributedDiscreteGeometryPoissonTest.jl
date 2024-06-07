1ac42f2b57551c94c9ae2132fa3a0bccfe07b8e2
zjwegert <60646897+zjwegert@users.noreply.github.com>
2024-06-07 17:24:37 +1000

    Fix test

--- test/DistributedTests/sequential/DistributedDiscreteGeometryPoissonTest.jl
+++ test/DistributedTests/sequential/DistributedDiscreteGeometryPoissonTest.jl
@@ -2,7 +2,7 @@ module DistributedDiscreteGeometryPoissonTestsSeq
 using PartitionedArrays
 include("../DistributedDiscreteGeometryPoissonTest.jl")
 with_debug() do distribute
-    PoissonTests.main(distribute,(2,2))
-    PoissonTests.main(distribute,(4,1),cells=(12,12),geometry=:remotes)
+  DistributedDiscreteGeometryPoissonTest.main(distribute,(2,2))
+  DistributedDiscreteGeometryPoissonTest.main(distribute,(4,1),cells=(12,12),geometry=:remotes)
 end
 end
