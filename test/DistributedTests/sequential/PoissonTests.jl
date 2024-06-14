b487bd0b4feb2df98400ea20cc59b8d65848ac1a
Eric Neiva <eric.miranda-neiva@college-de-france.fr>
2024-06-14 18:11:15 +0200

    Added failing test

--- test/DistributedTests/sequential/PoissonTests.jl
+++ test/DistributedTests/sequential/PoissonTests.jl
@@ -2,7 +2,8 @@ module PoissonTestsSeq
 using PartitionedArrays
 include("../PoissonTests.jl")
 with_debug() do distribute
-    PoissonTests.main(distribute,(2,2))
-    PoissonTests.main(distribute,(4,1),cells=(12,12),geometry=:remotes)
+    PoissonTests.main(distribute,(2,2)) # Passes test
+    PoissonTests.main(distribute,(2,4)) # Fails test (L2 and H1 errors above tolerances)
+    # PoissonTests.main(distribute,(4,1),cells=(12,12),geometry=:remotes)
 end
 end
