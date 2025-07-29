e0cca56f14167cacb1560e00c7fed31a8dab4081
Eric Neiva <eric.miranda-neiva@college-de-france.fr>
2025-07-29 18:40:03 +0200

    Refactored distributed CA for objective function

--- test/dev/distributed_aggregation.jl
+++ test/dev/distributed_aggregation.jl
@@ -13,7 +13,6 @@ function generate_dumbell(ranks, np, nc, ng)
   pmax = Point(L/4,3*L/8)
 
   R = 0.2
-  x = VectorValue(1.0,0.0)
   h = L/(2*nc)
 
   geo1 = disk(R,x0=p0)
@@ -44,33 +43,51 @@ function exchange_impl!(vector_partition,cache)
   return t
 end
 
-function find_optimal_roots!(lcell_to_root,lcell_to_value,cell_indices)
+function find_optimal_roots!(lcell_to_root,lcell_to_path_length,lcell_to_bbox_diam,cell_indices)
   # Bring all root candidates to the owner of the cut cell
   roots_cache = PartitionedArrays.p_vector_cache(lcell_to_root,cell_indices)
   t1 = exchange_impl!(lcell_to_root,roots_cache)
-  values_cache = PartitionedArrays.p_vector_cache(lcell_to_value,cell_indices)
-  t2 = exchange_impl!(lcell_to_value,values_cache)
+  lengths_cache = PartitionedArrays.p_vector_cache(lcell_to_path_length,cell_indices)
+  t2 = exchange_impl!(lcell_to_path_length,lengths_cache)
+  bb_diams_cache = PartitionedArrays.p_vector_cache(lcell_to_bbox_diam,cell_indices)
+  t3 = exchange_impl!(lcell_to_bbox_diam,bb_diams_cache)
   lcell_to_owner = map(copy∘local_to_owner,cell_indices)
   owners_cache = PartitionedArrays.p_vector_cache(lcell_to_owner,cell_indices)
   wait(t1)
   wait(t2)
+  wait(t3)
 
   # Select the optimal root for each local cut cell
   map(
-    lcell_to_root,lcell_to_value,lcell_to_owner,roots_cache,values_cache
-  ) do lcell_to_root, lcell_to_value, lcell_to_owner, roots_cache, values_cache
+    lcell_to_root,lcell_to_path_length,lcell_to_bbox_diam,
+      lcell_to_owner,roots_cache,lengths_cache,bb_diams_cache
+  ) do lcell_to_root,lcell_to_path_length,lcell_to_bbox_diam,
+      lcell_to_owner,roots_cache,lengths_cache,bb_diams_cache
+
     lids_rcv = roots_cache.local_indices_rcv
-    values_rcv = values_cache.buffer_rcv
+    lengths_rcv = lengths_cache.buffer_rcv
+    bb_diams_rcv = bb_diams_cache.buffer_rcv
     roots_rcv = roots_cache.buffer_rcv
-    for (k, nbor) in enumerate(roots_cache.neighbors_rcv)
-      for (lcell,root,value) in zip(lids_rcv[k], roots_rcv[k], values_rcv[k])
-        if lcell_to_value[lcell] > value # Take the minimum
+
+    for (k,nbor) in enumerate(roots_cache.neighbors_rcv)
+      for (lcell,root,len,bb_diam) in zip(lids_rcv[k],roots_rcv[k],lengths_rcv[k],bb_diams_rcv[k])
+        if lcell_to_path_length[lcell] > len
           lcell_to_root[lcell] = root
-          lcell_to_value[lcell] = value
           lcell_to_owner[lcell] = nbor
+        elseif lcell_to_path_length[lcell] == len
+          if ( lcell_to_bbox_diam[lcell] > bb_diam ) &
+              !isapprox(lcell_to_bbox_diam[lcell],bb_diam,atol=1.0e-9)
+            lcell_to_root[lcell] = root
+            lcell_to_owner[lcell] = nbor
+          elseif isapprox(lcell_to_bbox_diam[lcell],bb_diam,atol=1.0e-9) &
+              ( lcell_to_root[lcell] > root )
+            lcell_to_root[lcell] = root
+            lcell_to_owner[lcell] = nbor
+          end
         end
       end
     end
+
   end
 
   # Scatter the optimal roots and values
@@ -78,7 +95,6 @@ function find_optimal_roots!(lcell_to_root,lcell_to_value,cell_indices)
   # we are done debugging
   t1 = consistent!(PVector(lcell_to_owner,cell_indices,owners_cache))
   t2 = consistent!(PVector(lcell_to_root,cell_indices,roots_cache))
-  t3 = consistent!(PVector(lcell_to_value,cell_indices,values_cache))
   
   wait(t1)
 
@@ -87,52 +103,29 @@ function find_optimal_roots!(lcell_to_root,lcell_to_value,cell_indices)
   end
 
   wait(t2)
-  wait(t3)
 
-  return lcell_to_root, lcell_to_value, agg_cell_indices
+  return lcell_to_root, lcell_to_owner, agg_cell_indices
 end
-
 
 distribute = PartitionedArrays.DebugArray
 np = (2,1)
 ranks = distribute(LinearIndices((prod(np),)))
-bgmodel, geo = generate_dumbell(ranks, np, 8, 3)
+bgmodel, geo = generate_dumbell(ranks, np, 8, 2)
 cutgeo = cut(bgmodel, geo)
 
 cell_indices = partition(get_cell_gids(bgmodel))
 
 strategy = AggregateCutCellsByThreshold(1.0)
-lcell_to_lroot = map(local_views(cutgeo)) do cutgeo
-  aggregate(strategy,cutgeo,geo,IN)
-end
+lcell_to_root, lcell_to_path_length, lcell_to_bbox_diam =
+  map(local_views(cutgeo),cell_indices) do cutgeo,cell_indices
+    lid_to_gid = local_to_global(cell_indices)
+    aggregate(strategy,cutgeo,geo,lid_to_gid,IN)
+  end |> tuple_of_arrays
 
-# This would ideally come from the serial algorithm, but we simulate it here
-lcell_to_value = map(local_views(bgmodel),lcell_to_lroot) do bgmodel, lcell_to_lroot
-  lcell_centroid = lazy_map(mean, get_cell_coordinates(bgmodel))
-  lcell_to_value = fill(Inf, num_cells(bgmodel))
-  for (lcell, lroot) in enumerate(lcell_to_lroot)
-    if !iszero(lroot)
-      value = norm(lcell_centroid[lcell] - lcell_centroid[lroot])
-      lcell_to_value[lcell] = value
-    end
-  end
-  return lcell_to_value
-end
-lcell_to_value_copy = map(copy,lcell_to_value)
+lcell_to_inconsistent_root = map(copy,lcell_to_root)
 
-# This can also be done in place, but we duplicate info for now
-lcell_to_root = map(lcell_to_lroot,cell_indices) do lcell_to_lroot, cell_indices
-  lcell_to_root = fill(0, length(cell_indices))
-  lid_to_gid = local_to_global(cell_indices)
-  for i in eachindex(lcell_to_root)
-    if !iszero(lcell_to_lroot[i])
-      lcell_to_root[i] = lid_to_gid[lcell_to_lroot[i]]
-    end
-  end
-  return lcell_to_root
-end
-
-lcell_to_root, lcell_to_value, agg_cell_indices = find_optimal_roots!(lcell_to_root,lcell_to_value,cell_indices);
+lcell_to_root, lcell_to_owner, agg_cell_indices =
+  find_optimal_roots!(lcell_to_root,lcell_to_path_length,lcell_to_bbox_diam,cell_indices);
 
 # Creating aggregate-conforming cell partition
 
@@ -140,23 +133,31 @@ gids = get_cell_gids(bgmodel)
 ocell_to_root = map(lcell_to_root,own_to_local(gids)) do agg,o_to_l
   map(Reindex(agg),o_to_l)
 end
-ocell_to_lroot = map(lcell_to_lroot,own_to_local(gids)) do agg,o_to_l
-  map(Reindex(agg),o_to_l)
-end
 
 writevtk(EmbeddedBoundary(cutgeo),"data/bnd");
 writevtk(
   Triangulation(bgmodel), "data/dumbell_aggregates", 
-  celldata = ["aggregate" => ocell_to_root, "local_aggregates" => ocell_to_lroot],
+  celldata = ["aggregate" => ocell_to_root],
 );
 
-map(ranks,local_views(bgmodel),lcell_to_lroot,lcell_to_value_copy) do r,bgmodel, lcell_to_lroot, lcell_to_value
+map(ranks,
+    local_views(bgmodel),
+    lcell_to_root,
+    lcell_to_inconsistent_root,
+    lcell_to_owner,
+    lcell_to_path_length,
+    lcell_to_bbox_diam) do r,bgmodel,
+                          lcell_to_root,
+                          lcell_to_iroot,
+                          lcell_to_owner,
+                          lcell_to_path_length,
+                          lcell_to_bbox_diam
   writevtk(
     Triangulation(bgmodel), "data/dumbell_aggregates_$(r)", 
-    celldata = ["values" => lcell_to_value, "roots" => lcell_to_lroot],
+    celldata = [ "roots"              => lcell_to_root,
+                 "inconsistent roots" => lcell_to_iroot,
+                 "owners"             => lcell_to_owner,
+                 "length"             => lcell_to_path_length,
+                 "diameter"           => lcell_to_bbox_diam ],
   );
 end
-
-
-
-
